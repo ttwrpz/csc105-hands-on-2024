aef790e0e91ae3fd39b5b1e0114f7fa70e5c3b0b
ttwrpz <smrmakeyt@gmail.com>
2025-03-26 22:14:38 +0700

    feat: Added A04 - Profile Website icons for demo

--- A04 - Profile Website/src/components/Navbar.jsx
+++ A04 - Profile Website/src/components/Navbar.jsx
@@ -1,18 +1,22 @@
 import React from "react";
+import { IconHome, IconLibraryPhoto, IconSparkles } from "@tabler/icons-react";
 
 function Navbar() {
   return (
     <nav className="flex flex-col items-center justify-between gap-8 bg-gray-900 px-8 py-4 text-white shadow-sm sm:flex-row sm:py-2">
       <h1 className="font-semibold">Champ</h1>
       <div className="flex flex-col gap-8 text-center text-sm sm:flex-row">
-        <a className="transition hover:scale-105" href="#home">
-          Home
+        <a className="transition hover:scale-105 flex flex-row gap-2 items-center justify-center" href="#home">
+          <IconHome className="size-5"/>
+          <span>Home</span>
         </a>
-        <a className="transition hover:scale-105" href="#about">
-          About Me
+        <a className="transition hover:scale-105 flex flex-row gap-2 items-center justify-center" href="#about">
+          <IconSparkles className="size-5"/>
+          <span>About Me</span>
         </a>
-        <a className="transition hover:scale-105" href="#gallery">
-          Gallery
+        <a className="transition hover:scale-105 flex flex-row gap-2 items-center justify-center" href="#gallery">
+          <IconLibraryPhoto className="size-5"/>
+          <span>Gallery</span>
         </a>
       </div>
       <a className="rounded bg-green-800 px-4 py-2 text-white transition hover:scale-105">
